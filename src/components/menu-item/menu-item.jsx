76b2534083873d861cf0008e35dfb5ea050c8f81
Usmonjon001 <tokhirjonovusmonjon@gmail.com>
2021-12-28 22:26:23 +0500

    Finishing menu Links

---
+++ src/components/menu-item/menu-item.jsx
@@ -0,0 +1,19 @@
+import './menu-item.scss'
+
+const MenuItem = ({title, imageUrl, size}) =>{
+    return (
+        <div className={`${size} menu-item`}>
+
+            <div className={`${size} background-image`}
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}/>
+            <div className="content">
+                <h1 className="title">{ title.toUpperCase() }</h1>
+                <span className="subtitle">SHOW NOW</span>         
+            </div>
+        </div>
+    );
+}
+
+export default MenuItem;
